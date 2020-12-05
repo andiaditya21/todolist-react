bd85721018ab906abfed586bf4cf979edc5d3229
andiaditya21 <andiaditya21@gmail.com>
2020-12-05 13:51:00 +0800

    section header dgn child btn dan title beserta stylingnya

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <div className="frame"></div>
+      <div className="frame">
+        <div className="header">
+          <button className="header-btn">Add</button>
+          <div className="header-title">Title TodoList</div>
+          <button className="header-btn main-red-color">Clear</button>
+        </div>
+      </div>
     </div>
   );
 }
