4291f5c38cd9803a5026a2d79cf5c60ff5729372
andiaditya21 <andiaditya21@gmail.com>
2020-12-05 15:25:13 +0800

    perbaiki index dan app js, change css dari index->App css kemudian menghapus index.css. menambahkan filel2 komponen

--- src/App.js
+++ src/App.js
@@ -6,15 +6,39 @@ function App() {
     <div className="container">
       <div className="frame">
         <section className="header">
-          <button className="header-btn">Add</button>
+          <button className="header-btn main-black-color">Add</button>
           <div className="header-title">Title TodoList</div>
           <button className="header-btn main-red-color">Clear</button>
         </section>
         <section className="add">
           <form action="" className="add-form">
             <input type="text" className="add-input" />
-            <button className="add-btn">Add</button>
+            <button className="add-btn main-black-color">Add</button>
           </form>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
+        </section>
+        <section className="todos">
+          <div className="todo">Learn React</div>
         </section>
       </div>
     </div>
