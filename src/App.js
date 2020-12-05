8ec2034aba5c7f1173275a7f3db5e6c1b022fcc1
andiaditya21 <andiaditya21@gmail.com>
2020-12-05 16:23:38 +0800

    mengisi Paper.jsx dan memanggilnya di App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
-import logo from "./logo.svg";
+import Paper from "./component/Paper";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color">Add</button>
-          <div className="header-title">Title TodoList</div>
-          <button className="header-btn main-red-color">Clear</button>
-        </section>
-        <section className="add">
-          <form action="" className="add-form">
-            <input type="text" className="add-input" />
-            <button className="add-btn main-black-color">Add</button>
-          </form>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-        <section className="todos">
-          <div className="todo">Learn React</div>
-        </section>
-      </div>
-    </div>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">Add</button>
+        <div className="header-title">Title TodoList</div>
+        <button className="header-btn main-red-color">Clear</button>
+      </section>
+      <section className="add">
+        <form action="" className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
+      <section className="todos">
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+        <div className="todo">Learn React</div>
+      </section>
+    </Paper>
   );
 }
 
